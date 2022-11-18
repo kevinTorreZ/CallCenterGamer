28f7b11f223f14b6cf0d3bbc645193c78ada0759
kevinTorreZ <61859021+kevinTorreZ@users.noreply.github.com>
2022-11-18 13:37:14 -0300

    asd
    asd

--- CallCenterGamer/online/views.py
+++ CallCenterGamer/online/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def Index(request):
+    return render(request, "index.html")
+def Login(request):
+    return render(request, "login.html")
+def Register(request):
+    return render(request, "register.html")
+def Inicio(request):
+    return render(request, "idnicio.html")
