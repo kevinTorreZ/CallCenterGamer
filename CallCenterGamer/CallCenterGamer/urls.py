b2980074efc2f78cf26843312ce99fc1fd609601
kevinTorreZ <61859021+kevinTorreZ@users.noreply.github.com>
2022-11-18 13:31:05 -0300

    Update urls.py

--- CallCenterGamer/CallCenterGamer/urls.py
+++ CallCenterGamer/CallCenterGamer/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from online.views import Index,Inicio,Register,Login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Index)
 ]
