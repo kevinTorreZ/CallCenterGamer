653960a3bc288c82bb8470d6ffc49ccf0c5b6e8e
MatLumber <matidones5@gmail.com>
2022-11-24 21:46:10 -0300

    afa

--- online/forms.py
+++ online/forms.py
@@ -64,7 +64,7 @@ class RespuestaForm(forms.ModelForm):
         fields = ['Respuesta','id_Pregunta','Usuario','FechaHora']
         widgets = {
             'Respuesta': forms.Textarea(
-                attrs={'placeholder': 'Ingrese su Respuesta'}),
+                attrs={'placeholder': ''}),
             'id_Pregunta': forms.HiddenInput(),
             'Usuario': forms.HiddenInput(),
             'FechaHora': forms.HiddenInput(),
